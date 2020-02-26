5d382bff21778a7c68d9bc6977cdbf4335f77dae
Cerber2ol8 <41719978+Cerber2ol8@users.noreply.github.com>
2020-02-27 01:02:33 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,8 @@
-echo download new script>>update_log
-wget https://raw.githubusercontent.com/Cerber2ol8/scripts_update/master/task.sh
-sudo bash ./stop.sh
-rm -f tmp.py
-wget https://raw.githubusercontent.com/Cerber2ol8/scripts_update/master/tmp.py
-sudo bash ./start.sh
+#echo download new script>>update_log
+#wget https://raw.githubusercontent.com/Cerber2ol8/scripts_update/master/task.sh
+#sudo bash ./stop.sh
+#rm -f tmp.py
+#wget https://raw.githubusercontent.com/Cerber2ol8/scripts_update/master/tmp.py
+#sudo bash ./start.sh
 
+echo `date`>>update_log

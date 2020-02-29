2a411a7e218bbdd3f1979dcf8661c10bf2450e9c
Cerber2ol8 <41719978+Cerber2ol8@users.noreply.github.com>
2020-02-29 22:10:00 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,6 +5,10 @@ sudo bash ./stop.sh
 rm -f tmp.py
 wget https://gitee.com/Cerber2ol8/scripts_update/raw/master/tmp.py
 
+rm -f start.sh
+rm -f out
+wget https://gitee.com/Cerber2ol8/scripts_update/raw/master/start.py
+
 cp /tmp/version.txt version.txt
 echo update at>>update_log
 echo `date`>>update_log

87e80aa0ac3c7ac2ae4a9e929749a642ed88d176
Cerber2ol8 <41719978+Cerber2ol8@users.noreply.github.com>
2020-03-01 01:34:02 +0800

    Add files via upload

--- tmp.py
+++ tmp.py
@@ -87,7 +87,7 @@ if __name__ == '__main__':
     #schedule.every(5).minutes.do(check_update)
     schedule.every().day.at("08:04").do(task_report)
     schedule.every().day.at("00:12").do(task_report)
-    schedule.every().day.at("01:30").do(task_report)
+
     while True:
         schedule.run_pending()
 

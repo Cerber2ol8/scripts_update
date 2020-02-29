dcf438ae7535b3300f6df960f1919538e6c6c4db
Cerber2ol8 <41719978+Cerber2ol8@users.noreply.github.com>
2020-03-01 00:12:47 +0800

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -80,10 +80,10 @@ if __name__ == '__main__':
 
     save_pid(PID_FILE)
     check_update()
-    schedule.every(120).seconds.do(check_update)
+    schedule.every(60).seconds.do(check_update)
     #schedule.every(5).minutes.do(check_update)
     schedule.every().day.at("08:04").do(task_report)
-    schedule.every().day.at("00:12").do(task_report)
+    schedule.every().day.at("00:20").do(task_report)
     while True:
         schedule.run_pending()
 

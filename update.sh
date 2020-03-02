546cf994e13a4f935ec619ac3b890a3539d732be
Cerber2ol8 <41719978+Cerber2ol8@users.noreply.github.com>
2020-03-02 20:42:58 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,13 +3,13 @@ echo download new script>>update_log
 
 sudo bash ./stop.sh
 rm -f tmp.py
-#wget --timeout=10 --waitretry=2 --tries=3 https://gitee.com/Cerber2ol8/scripts_update/raw/master/tmp.py
-wget --timeout=10 --waitretry=2 --tries=3 https://raw.githubusercontent.com/Cerber2ol8/scripts_update/master/tmp.py
+#wget --waitretry=2  https://gitee.com/Cerber2ol8/scripts_update/raw/master/tmp.py
+wget --waitretry=2 https://raw.githubusercontent.com/Cerber2ol8/scripts_update/master/tmp.py
 
 #rm -f start.sh
 rm -f print.out
-#wget --timeout=10 --waitretry=2 --tries=3 https://gitee.com/Cerber2ol8/scripts_update/raw/master/start.sh
-#wget --timeout=10 --waitretry=2 --tries=3 https://raw.githubusercontent.com/Cerber2ol8/scripts_update/master/start.sh
+#wget --waitretry=2 https://gitee.com/Cerber2ol8/scripts_update/raw/master/start.sh
+#wget --waitretry=2 https://raw.githubusercontent.com/Cerber2ol8/scripts_update/master/start.sh
 
 cp /tmp/version.txt version.txt
 echo update at>>update_log

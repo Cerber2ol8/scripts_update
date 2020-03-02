037064a3846c6f5967f798e7a571c0e267ae812b
Cerber2ol8 <41719978+Cerber2ol8@users.noreply.github.com>
2020-03-02 20:05:38 +0800

    Add files via upload

--- tmp.py
+++ tmp.py
@@ -5,6 +5,7 @@ import sys
 from datetime import datetime
 import logging
 import time
+import testmail
 
 LOG_FILE = '/home/mytools.log'
 PID_FILE = '/tmp/tools.pid'
@@ -23,7 +24,7 @@ def check_update():
     if not os.path.exists('version.txt'):
         try:
             print('Getting version ...')
-            os.system('wget https://raw.githubusercontent.com/Cerber2ol8/scripts_update/master/version.txt')
+            os.system('wget --timeout=10 --waitretry=2 --tries=3 https://raw.githubusercontent.com/Cerber2ol8/scripts_update/master/version.txt')
             update()
 
         finally:
@@ -35,7 +36,7 @@ def check_update():
             if os.path.exists('/tmp/version.txt'):
                 os.remove('/tmp/version.txt')
                 time.sleep(1)
-            os.system('wget -P /tmp https://raw.githubusercontent.com/Cerber2ol8/scripts_update/master/version.txt')
+            os.system('wget -P --timeout=10 --waitretry=2 --tries=3 /tmp https://raw.githubusercontent.com/Cerber2ol8/scripts_update/master/version.txt')
             if os.path.exists('/tmp/version.txt'):
                 with open('/tmp/version.txt', 'r') as f1:
                     lastest_version = f1.read()
@@ -51,7 +52,7 @@ def update():
     if os.path.exists('update.sh'):
         os.remove('update.sh')
     time.sleep(1)
-    os.system('wget https://raw.githubusercontent.com/Cerber2ol8/scripts_update/master/update.sh')
+    os.system('wget --timeout=10 --waitretry=2 --tries=3 https://raw.githubusercontent.com/Cerber2ol8/scripts_update/master/update.sh')
     os.system("echo "+datetime.now().strftime("%Y-%m-%d %H:%M:%S")+">>log")
     os.system('echo last update update>>log')
     os.system('sudo bash ./update.sh')
@@ -71,7 +72,8 @@ def task_spider():
     os.system('python spider.py')
 
 def task_testmail():
-    os.system('python testmail.py')
+    testmail.sendMaill("test","test")
+
 
 if __name__ == '__main__':
     if os.path.exists(PID_FILE):

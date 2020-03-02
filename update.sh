c420ac35668fb0e4368246fb9d022147f6e1e3c2
Cerber2ol8 <41719978+Cerber2ol8@users.noreply.github.com>
2020-03-02 20:06:10 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,15 +1,15 @@
 echo download new script>>update_log
-#wget https://gitee.com/Cerber2ol8/scripts_update/raw/master/task.sh
+#wget --timeout=10 --waitretry=2 --tries=3 https://gitee.com/Cerber2ol8/scripts_update/raw/master/task.sh
 
 sudo bash ./stop.sh
 rm -f tmp.py
-#wget https://gitee.com/Cerber2ol8/scripts_update/raw/master/tmp.py
-wget https://raw.githubusercontent.com/Cerber2ol8/scripts_update/master/tmp.py
+#wget --timeout=10 --waitretry=2 --tries=3 https://gitee.com/Cerber2ol8/scripts_update/raw/master/tmp.py
+wget --timeout=10 --waitretry=2 --tries=3 https://raw.githubusercontent.com/Cerber2ol8/scripts_update/master/tmp.py
 
 #rm -f start.sh
 rm -f print.out
-#wget https://gitee.com/Cerber2ol8/scripts_update/raw/master/start.sh
-#wget https://raw.githubusercontent.com/Cerber2ol8/scripts_update/master/start.sh
+#wget --timeout=10 --waitretry=2 --tries=3 https://gitee.com/Cerber2ol8/scripts_update/raw/master/start.sh
+#wget --timeout=10 --waitretry=2 --tries=3 https://raw.githubusercontent.com/Cerber2ol8/scripts_update/master/start.sh
 
 cp /tmp/version.txt version.txt
 echo update at>>update_log
